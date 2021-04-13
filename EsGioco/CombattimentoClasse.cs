c6d47cf85a9c894658ef261d838fd159fab5e60f
leonardo969 <leonardo.vincenzi.stud@ispascalcomandini.it>
2021-04-13 10:49:30 +0200

    Modifiche diagramma delle classi 13/04/21

--- EsGioco/CombattimentoClasse.cs
+++ EsGioco/CombattimentoClasse.cs
@@ -7,5 +7,77 @@ namespace EsGioco
 {
     public class CombattimentoClasse
     {
+        public Personaggio P1
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public Personaggio P2
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public Arma ArmaP1
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public Arma ArmaP2
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public Personaggio Vincitore
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public Personaggio Perdente
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void AttaccoP1()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AttaccoP2()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SchivaP1()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SchivaP2()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void TogliVita()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
